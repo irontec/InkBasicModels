f5cfe20965200122679a397404a1765f9838c9ea
Inlacou <inlacou@gmail.com>
2019-08-16 11:12:38 +0200

    basic demo app

--- app/src/main/java/com/inlacou/inkbasicmodelslibraryproject/MainActivity.kt
+++ app/src/main/java/com/inlacou/inkbasicmodelslibraryproject/MainActivity.kt
@@ -1,12 +1,65 @@
 package com.inlacou.inkbasicmodelslibraryproject
 
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
+import com.inlacou.inkbasicmodels.*
+import com.inlacou.inkbasicmodels.extensions.applyModel
+import com.inlacou.inkspannable.InkSpannableBuilder
+import com.inlacou.pripple.RippleButton
 
 class MainActivity : AppCompatActivity() {
 	
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+		
+		findViewById<LinearLayout>(R.id.linearlayout)?.applyModel(GeneralViewMdl(
+				paddingBottom = 16.dpToPx(),
+				paddingLeft = 16.dpToPx(),
+				paddingRight = 16.dpToPx(),
+				paddingTop = 16.dpToPx()
+		))
+		
+		findViewById<TextView>(R.id.textview)?.applyModel(TextViewMdl(
+				textMdl = TextMdl(
+						text = InkSpannableBuilder().addText("Example text with some").addBlank().addTextBold("bold"),
+						textColorResId = R.color.colorAccent,
+						textSizeSp = 20f
+				),
+				generalViewMdl = GeneralViewMdl(
+						onClick = { Toast.makeText(this, "You clicked me!", Toast.LENGTH_SHORT).show(); true},
+						paddingBottom = 16.dpToPx(),
+						paddingLeft = 16.dpToPx(),
+						paddingRight = 16.dpToPx(),
+						paddingTop = 16.dpToPx()
+				)
+		))
+		
+		findViewById<RippleButton>(R.id.ripplebutton)?.applyModel(RippleButtonViewMdl(
+				textMdl = TextMdl(
+						text = InkSpannableBuilder().addText("Button"),
+						textSizeSp = 20f
+				),
+				generalViewMdl = GeneralViewMdl(
+						onClick = { Toast.makeText(this, "You clicked me!", Toast.LENGTH_SHORT).show(); true},
+						paddingBottom = 16.dpToPx(),
+						paddingLeft = 16.dpToPx(),
+						paddingRight = 16.dpToPx(),
+						paddingTop = 16.dpToPx()
+				),
+				rippleViewMdl = RippleMdl(
+						normalColorResId = R.color.colorPrimary,
+						rippleColorResId = R.color.colorPrimaryDark,
+						cornerRadius = 16f,
+						strokeWidth = 1.dpToPx(),
+						strokeColorResId = R.color.colorAccent
+				)
+		))
 	}
+	
+	private fun Int.dpToPx() = (this * Resources.getSystem().displayMetrics.density).toInt()
 }
